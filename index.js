adc708527be4dd899e60d0f377dbcbfac401eb5f
wei.ding <wei.ding@datayes.com>
2016-07-04 14:57:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fallback = require('express-history-api-fallback');
+var logger = require('./src/logger');
+
+var app = express();
+
+app.use(express.static('assets'));
+// history api fallback
+app.use(fallback('index.html', {
+    root: `${__dirname}/assets`
+}));
+
+// app.use(express.static('assets/dist'));
+// app.use(fallback('index.html', {
+//     root: `${__dirname}/assets/dist`
+// }));
+
+app.listen(3000, function() {
+    logger.info(`Backend service listening on port 3000!`);
+});
